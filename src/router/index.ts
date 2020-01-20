51739db8a002999601eb35479e7c10a571482c59
Nathaniel Long <nathaniel.s.long@gmail.com>
2020-01-20 12:19:02 -0500

    adding basic routes (#2)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Budget from "../views/Budget.vue";
+import NewBudget from "../views/NewBudget.vue";
 
 Vue.use(VueRouter);
 
@@ -11,17 +13,14 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
-  },
-  {
     path: "/budget",
     name: "budget",
-    component: () => import(/* webpackChunkName: "about" */ "../views/Budget.vue")
+    component: Budget
+  },
+  {
+    path: "/budget/new",
+    name: "newbudget",
+    component: NewBudget
   }
 ];
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello home</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
 };
 </script>
